53f2c9a9ee2caa521baff61e8e7a516005007a5e
jenda <ja.jan.ja@seznam.cz>
2022-12-04 18:48:15 +0100

    new deploy scripy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 git fetch
 git pull
-
-docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.yaml up -d
+docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml stop
+docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml build
+docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml up -d
