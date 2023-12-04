12b29965e7dbcd62409cc692b4365423a80c95f3
Jan Docekal <jan.docekal@nanoenergies.cz>
2023-12-04 10:33:30 +0100

    troubleshooting

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 git fetch
 git pull
 docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml stop
-docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml pull
+docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml build
 docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml up -d
