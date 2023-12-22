5a2b71f6db055d593076b4bf49ebd979343cc169
Jan Docekal <jan.docekal@nanoenergies.cz>
2023-12-22 15:52:13 +0100

    optimization

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git pull
 docker-compose stop myhomeautomation.webapp myhomeautomation.webapi
 docker-compose rm -f myhomeautomation.webapp myhomeautomation.webapi
 docker-compose pull myhomeautomation.webapp myhomeautomation.webapi
-docker-compose -f ./docker-compose.override.yaml -f ./docker-compose.ArmV7.yaml up -d
+docker-compose -f ./docker-compose.override.yaml up -d
